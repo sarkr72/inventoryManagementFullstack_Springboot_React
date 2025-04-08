4fdc4e526527ab277478073461fb79bcf33145ca
rinku sarkar <54875424+sarkr72@users.noreply.github.com>
2025-04-08 16:16:51 -0400

    Update S3Service.java

--- ims-backend-master/src/main/java/com/IMS_Backend/ims_backend/servicesImplementations/S3Service.java
+++ ims-backend-master/src/main/java/com/IMS_Backend/ims_backend/servicesImplementations/S3Service.java
@@ -1,28 +1,34 @@
 package com.IMS_Backend.ims_backend.servicesImplementations;
 
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.IMS_Backend.ims_backend.model.Company;
+import com.IMS_Backend.ims_backend.repository.CompanyRepository;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
-import java.io.File;
-import java.time.LocalDate;
 
 @Service
 public class S3Service {
 
     private final AmazonS3 amazonS3;
     private final String bucketName;
-
+    
+	@Autowired
+	private CompanyRepository companyRepository;
+	
     public S3Service(
         @Value("${aws.accessKeyId}") String accessKeyId,
         @Value("${aws.secretKey}") String secretKey,
         @Value("${aws.region}") String region,
-        @Value("${aws.s3.bucket.name}") String bucketName
+        @Value("${aws.s3.bucket.name}") String bucketName, CompanyRepository companyRepository
     ) {
         this.bucketName = bucketName;
 
@@ -31,11 +37,14 @@ public class S3Service {
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
+        this.companyRepository = companyRepository;
     }
 
     public void uploadFile(File file, String companyId) {
         String filePath = String.format("%s/%s", companyId, file.getName());
         amazonS3.putObject(new PutObjectRequest(bucketName, filePath, file));
+        String fileUrl = generateFileUrl(companyId, file.getName()); // Generate the URL
+        saveFileUrlToDatabase(fileUrl);
     }
 
     public S3Object downloadFile(String companyId,  String fileName) {
@@ -47,4 +56,15 @@ public class S3Service {
         String filePath = String.format("%s/%s", companyId, fileName);
         amazonS3.deleteObject(bucketName, filePath);
     }
+    
+    public String generateFileUrl(String companyId, String fileName) {
+        String filePath = String.format("%s/%s", companyId, fileName);
+        return amazonS3.getUrl(bucketName, filePath).toString();
+    }
+    
+    public void saveFileUrlToDatabase(String fileUrl) {
+        Company cp = new Company("company1", "add1", "con1", fileUrl);
+        companyRepository.save(cp);
+    }
+    
 }
