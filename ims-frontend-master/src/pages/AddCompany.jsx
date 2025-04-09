9e2c4e3874eb3fdba2ccac283e4cdba113a9c142
rinku sarkar <54875424+sarkr72@users.noreply.github.com>
2025-04-09 11:30:41 -0400

    Update AddCompany.jsx

--- ims-frontend-master/src/pages/AddCompany.jsx
+++ ims-frontend-master/src/pages/AddCompany.jsx
@@ -15,6 +15,12 @@ const AddCompany = () => {
   const { updateCompany } = useParams();
   const navigate = useNavigate();
   const message = "Company";
+  const [file, setFile] = useState(null);
+  const [companyId, setCompanyId] = useState(localStorage.getItem("companyId"));
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
 
   useEffect(() => {
     if (updateCompany) {
@@ -41,12 +47,19 @@ const AddCompany = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!file) {
+      alert("Please select a logo first");
+      return;
+    }
+    
     if (updateCompany) {
-        updateCompanyWithId(data?.id, data).then((response) => {
+        updateCompanyWithId(data?.id, data).then(async(response) => {
+          await handleUpload();
           navigate("/manageCompanies");
         });
     } else {
-      createCompany(data).then((response) => {
+      createCompany(data).then(async(response) => {
+          await handleUpload();
           navigate("/manageCompanies");
         }).catch((error) => {
           console.log(error);
@@ -54,6 +67,33 @@ const AddCompany = () => {
     }
   };
 
+  const handleUpload = async () => {
+    if (!file) {
+      alert("Please select a logo first");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const response = await axios.post(
+        `http://localhost:8080/api/files/upload/${companyId}`, 
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      alert("File uploaded successfully!");
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      alert("Error uploading file");
+    }
+  };
 
 
   return (
@@ -65,6 +105,11 @@ const AddCompany = () => {
         handleSubmit={handleSubmit}
         handleChange={handleChange}
       />
+      <div>
+      <h3>Upload Company Logo:</h3>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload File</button>
+    </div>
     </div>
   );
 };
